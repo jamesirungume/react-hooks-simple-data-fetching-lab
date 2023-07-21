c50e10d09568a0a0790a7310dea1cea2e3dfcf15
Your Name <jymohcooler@gmail.com>
2023-07-21 14:49:18 +0300

    complete project

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogPic, setDogPic] = useState("");
+  
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogPic(data.message);
+        
+      })
+      .catch((error) => {
+        console.error("Error fetching dog picture:", error);
+        setLoadingPic(false);
+      });
+  }, []);
+
+  if (!dogPic) {
+    return <p>Loading...</p>;}
+
+  return (
+    <div>
+        <h1>Dog Finder</h1>
+        <img src={dogPic} alt="A Random Dog" />
+    </div>
+  )
+}
+
+export default App;
